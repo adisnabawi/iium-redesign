1a21b09ce2d63e2d5f15b0f4cd04a5b278af8530
Adis Nabawi <adis@Adiss-MacBook-Pro.local>
2021-01-12 18:17:13 +0800

    new

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ class Footer extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-                  footer1:[]
+                  footer1:[], footer2:[]
                  };
   }
 
@@ -24,12 +24,28 @@ class Footer extends React.Component {
           });
         }
       );
+    fetch("/json/footer/research_alumni.json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            footer2: result
+          });
+          console.log(this.state.footer2);
+
+        },
+        (error) => {
+          this.setState({
+            error
+          });
+        }
+      );
   }
   render() {
     return <div className="footer">
             <footer>
               <div className="row">
-                <div className="col-md-3">
+                <div className="col-md-4">
                   <p>
                   International Islamic University Malaysia <br />
                   P.O. Box 10, 50728 Kuala Lumpur <br />
@@ -40,7 +56,7 @@ class Footer extends React.Component {
                   Email : webmaster@iium.edu.my <br />
                   </p>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-2">
                   <p><b>Students & Parents</b></p>
                    <ul className="listFooter">
                    {this.state.footer1.map((item, i) => {
@@ -49,20 +65,31 @@ class Footer extends React.Component {
                    )}
                    </ul>
                 </div>
-               <div className="col-md-3">
-                 <ul>
-                    <li>Test 1 </li>
-                    <li>Test 2 </li>
-                    <li>Test 3 </li>
-                 </ul>
+               <div className="col-md-2">
+                 <p><b>Researchers, Lecturers & Alumni</b></p>
+                  <ul className="listFooter">
+                  {this.state.footer2.map((item, i) => {
+                    return( <li><a href={item.url}> {item.title} </a> </li>
+                    )}
+                  )}
+                  </ul>
                 </div>
-                <div className="col-md-3">
+                <div className="col-md-2">
+                  <p><b>Business, Government & Related Links</b></p>
                   <ul>
                      <li>Test 1 </li>
                      <li>Test 2 </li>
                      <li>Test 3 </li>
                   </ul>
                  </div>
+                 <div className="col-md-2">
+                   <p><b>Others</b></p>
+                   <ul>
+                      <li>Test 1 </li>
+                      <li>Test 2 </li>
+                      <li>Test 3 </li>
+                   </ul>
+                  </div>
               </div>
               </footer>
           </div>
